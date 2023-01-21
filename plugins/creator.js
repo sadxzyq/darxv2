66dfb348749561c3947b2a81e8f27b160e17d023
sadxzyq <satriopambudi866@gmail.com>
2023-01-21 12:21:51 +0700

    darxv2

--- plugins/creator.js
+++ plugins/creator.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, usedPrefix, text, args, command }) => {
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let name = await conn.getName(who)
 
-let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:WhatsApp;𝙳𝚊𝚛𝚔𝚡\nNICKNAME:👑 Owner 𝙳𝚊𝚛𝚔𝚡\nORG:NexEp\nTITLE:soft\nitem1.TEL;waid=6282285357346:+62 822-8535-7346\nitem1.X-ABLabel:📞 Nomor Owner\nitem2.URL:https://instagram.com/mydiam.is.fun\nitem2.X-ABLabel:💬 More\nitem3.EMAIL;type=INTERNET: satriopambudi866@gmail.com\nitem3.X-ABLabel:💌 Mail Owner 𝙳𝚊𝚛𝚔𝚡𝙱𝚘𝚝\nitem4.ADR:;;🇮🇩 Indonesia;;;;\nitem4.X-ABADR:💬 More\nitem4.X-ABLabel:📍 Lokasi Saya\nBDAY;value=date:🔖 11 Juli 2002\nEND:VCARD`
+let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:WhatsApp;𝙳𝚊𝚛𝚔𝚡\nNICKNAME:👑 Owner 𝙳𝚊𝚛𝚔𝚡\nORG:NexEp\nTITLE:soft\nitem1.TEL;waid=6282285357346:+62 822-8535-7346\nitem1.X-ABLabel:📞 Nomor Owner\nitem2.URL:https://instagram.com/mydiam.is.fun\nitem2.X-ABLabel:💬 More\nitem3.EMAIL;type=INTERNET: satriopambudi866@gmail.com\nitem3.X-ABLabel:💌 Mail Owner 𝙳𝚊𝚛𝚔𝚡𝙱𝚘𝚝\nitem4.ADR:;;🇮🇩 Indonesia;;;;\nitem4.X-ABADR:💬 More\nitem4.X-ABLabel:📍 Lokasi Saya\nBDAY;value=date:🔖 24 Mei 2023\nEND:VCARD`
 const tag_own = await conn.sendMessage(m.chat, { contacts: { displayName: wm, contacts: [{ vcard }] }}, { quoted: fkon })
 let caption = `👋 Hai *${name} @${who.split("@")[0]}*, Nih Owner *${conn.user.name}* kak`
     await conn.reply(m.chat, caption, tag_own, { mentions: conn.parseMention(caption) })
--- plugins/mode-bot.js
+++ plugins/mode-bot.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, args, command }) => {
 let totalf = Object.values(global.plugins).filter(
     (v) => v.help && v.tags
   ).length;
- await conn.sendButton(m.chat, `_🎐Pilih Mode bot nya kak..._\n_Off atau On di group_\n`,wm + '\n\n' + botdate, thumbdoc, [['🧿ON','.unbanchat'],['🎀OFF','.kannaoff']], m, {
+ await conn.sendButton(m.chat, `_🎐Pilih Mode bot nya kak..._\n_Off atau On di group_\n`,wm + '\n\n' + botdate, thumbdoc, [['🧿ON','.unbanchat'],['🎀OFF','.darkoff']], m, {
 contextInfo: { externalAdReply :{
                         mediaUrl: '',
                         mediaType: 2,
